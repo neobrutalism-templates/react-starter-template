20dd6a5e42e97c7a91391bab58d09f0eadb7e9e2
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-07-07 01:19:00 +0200

    remove theme switcher from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-import { ThemeSwitcher } from './components/theme-switcher'
-
 export default function App() {
-  return (
-    <div className="h-screen w-full bg-bg dark:bg-darkBg">
-      <ThemeSwitcher />
-    </div>
-  )
+  return <></>
 }
